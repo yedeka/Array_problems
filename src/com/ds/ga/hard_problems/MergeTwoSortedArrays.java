432aefd3dddef9d03ecbb9cd01f72a1ced2fd369
yedeka <yogesh.edekar@gmail.com>
2023-12-17 03:34:20 -0700

    Adding brute force solution for merge 2 sorted arrays

---
+++ src/com/ds/ga/hard_problems/MergeTwoSortedArrays.java
@@ -0,0 +1,60 @@
+package com.ds.ga.hard_problems;
+
+/**
+ * Problem statement: Given two sorted arrays arr1[] and arr2[] of sizes n and m in non-decreasing order.
+ * Merge them in sorted order. Modify arr1 so that it contains the first N elements and modify arr2 so that it contains the last M elements.
+ *
+ * Example 1:
+ *
+ * Input:
+ * n = 4, arr1[] = [1 4 8 10]
+ * m = 5, arr2[] = [2 3 9]
+ *
+ * Output:
+ * arr1[] = [1 2 3 4]
+ * arr2[] = [8 9 10]
+ *
+ * Brute Force - Just use a third array of size m + n to store the elements of both sorted arrays and then use the third array to populate elements.
+ * TC - O(m+n)
+ * SC - O(m+n)
+ *
+ * 
+ */
+public class MergeTwoSortedArrays {
+    private static int[] mergeSortedArraysBrute(int[] arr1, int[] arr2) {
+        int length1 = arr1.length;
+        int length2 = arr2.length;
+        int[] result = new int[length1 + length2];
+        int i = 0, j=0, k=0;
+        while(i < length1 && j < length2) {
+            if(arr1[i] <= arr2[j]) {
+                result[k++] = arr1[i++];
+            } else {
+                result[k++] = arr2[j++];
+            }
+        }
+        while(i < length1) {
+            result[k++] = arr1[i++];
+        }
+        while(j< length2) {
+            result[k++] = arr1[j++];
+        }
+        return result;
+    }
+
+    private static String printArray(int[] input){
+        String strElements = "[ ";
+        for(int element: input) {
+            strElements += element+", ";
+        }
+        strElements+= "]";
+        return strElements;
+    }
+    public static void main(String[] args) {
+        int[] arr1 = new int[]{1, 4, 8, 10};
+        int arr2[] = new int[]{2, 3, 9};
+        int[] merged = mergeSortedArraysBrute(arr1, arr2);
+        System.out.println("Merged Array => "+printArray(merged));
+        System.out.println("DONE");
+    }
+}
